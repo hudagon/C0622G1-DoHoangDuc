2b94c38ad274c7f8354707538e7037cc89b5293f
hudagon <dohoangduc1112000@gmail.com>
2022-09-15 17:21:40 +0700

    nopbai

--- module 3/caseStudy/dataBase/task11-15.sql
+++ module 3/caseStudy/dataBase/task11-15.sql
@@ -45,5 +45,19 @@ from hop_dong join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
               join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
               join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
 group by hop_dong_chi_tiet.ma_dich_vu_di_kem having so_lan_su_dung = 1
-order by hop_dong.ma_hop_dong;               
-	   
+order by hop_dong.ma_hop_dong;
+
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi
+-- mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.   
+select nhan_vien.ma_nhan_vien,
+	   nhan_vien.ho_ten,
+       trinh_do.ten_trinh_do,
+       bo_phan.ten_bo_phan,
+       nhan_vien.so_dien_thoai,
+       nhan_vien.dia_chi
+from nhan_vien join trinh_do on nhan_vien.ma_trinh_do = trinh_do.ma_trinh_do
+			   join bo_phan on nhan_vien.ma_bo_phan = bo_phan.ma_bo_phan
+               join hop_dong on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+where (hop_dong.ngay_lam_hop_dong between '2020-01-01' and '2021-12-31')
+group by nhan_vien.ma_nhan_vien
+having count(hop_dong.ma_nhan_vien) <= 3;
