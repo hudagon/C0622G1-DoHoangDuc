aa7b2651e61434085c8eb4bca10ac5311f118208
hudagon <dohoangduc1112000@gmail.com>
2022-11-24 00:36:05 +0700

    nopbai

--- module 5/case_study_one_last_time/cs-last/src/app/app.module.ts
+++ module 5/case_study_one_last_time/cs-last/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeFooterComponent } from './home-footer/home-footer.component';
+import { FacilityEditComponent } from './facility-edit/facility-edit.component';
+import { FacilityListComponent } from './facility-list/facility-list.component';
+import { FacilityCreateComponent } from './facility-create/facility-create.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeHeaderComponent
+    HomeHeaderComponent,
+    HomeFooterComponent,
+    FacilityEditComponent,
+    FacilityListComponent,
+    FacilityCreateComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule
